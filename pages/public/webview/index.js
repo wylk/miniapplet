ee8db82f30626d47bdcacc6601fadda4a38bd73f
hyy <lo_haoyuyan_ve@163.com>
2018-09-06 14:06:54 +0800

    添加返回按钮，加载两次页面

--- pages/public/webview/index.js
+++ pages/public/webview/index.js
@@ -6,27 +6,18 @@ Page({
     openid : ''
   },
   onShow: function(){
-    if (!t.data.webShowed) {
-      t.data.webShowed = true;
+    var a = getCurrentPages();
+    if (a.length == 1) {
       wx.navigateTo({
         url: '/pages/public/webview/index'
       })
     } 
-    // else {
-    //   wx.navigateBack({
-    //     data: 1
-    //   });
-    // }
-    var a = getCurrentPages();
-    $.alert(a);
-    // t.data.webShowed = true;
     this.getUserInfo();
   },
   getUserInfo: function () {
     var pageObj = this;
     wx.login({
       success: function (res) {
-        // console.log(res);
         var data = { code: res.code, store_id: 2 };
         $.post('wxapp/login', data, function (res) {
           // console.log(res);
@@ -43,15 +34,7 @@ Page({
   onShareAppMessage: function () {
 
   },
-  // bindViewTap:function(){
-  //   // wx.navigateBack({data:1});
-  //   // wx.setTabBarItem({
-  //   //   index: 0,
-  //   //   text: 'text',
-  //   //   iconPath: '/path/to/iconPath',
-  //   //   selectedIconPath: '/path/to/selectedIconPath'
-  //   // })
-  // },
+
   onTabItemTap:function(item){
     wx.setTabBarItem({
       index: 0,
